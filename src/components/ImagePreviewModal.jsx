23976b321674bbb2a69e8712d14a531238545599
SakibZaman255 <sakibzaman255@gmail.com>
2024-12-03 16:04:24 +0600

    Updated ImagePreviewModal

--- src/components/ImagePreviewModal.jsx
+++ src/components/ImagePreviewModal.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
 const ImagePreviewModal = ({ selectedImage, handleCloseModal }) => (
-  <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center" onClick={(e) => e.target === e.currentTarget && handleCloseModal()}>
-    <div className="bg-white p-8 rounded-lg shadow-md max-w-lg w-full text-center relative">
-      <button className="absolute top-4 right-4 text-xl font-bold text-gray-800" onClick={handleCloseModal}>
+  <div
+    className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 flex items-center justify-center"
+    onClick={(e) => e.target === e.currentTarget && handleCloseModal()}
+  >
+    <div className="bg-white p-8 rounded-lg shadow-md max-w-4xl w-full text-center relative">
+      <button
+        className="absolute top-4 right-4 text-2xl font-bold text-gray-800"
+        onClick={handleCloseModal}
+      >
         &times;
       </button>
       <h2 className="text-2xl font-semibold mb-6">Image Preview</h2>
-      <img src={selectedImage.url} alt={`Preview ${selectedImage.name}`} className="w-full h-56 object-cover rounded-lg mx-auto" />
-      <p className="text-lg font-medium mt-4 truncate">{selectedImage.name}</p>
+      <div className="max-h-[70vh] overflow-auto">
+        <img
+          src={selectedImage.url}
+          alt={`Preview ${selectedImage.name}`}
+          className="max-w-full h-auto mx-auto"
+        />
+      </div>
+      <p className="text-lg font-medium mt-4">{selectedImage.name}</p>
     </div>
   </div>
 );
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -22,7 +22,7 @@ const MainContent = ({ folders, selectedFolder, handleImageClick, handleFolderSe
                 {folder.folderName}
               </span>
             </h2>
-            <div className="grid grid-cols-9 gap-4">
+            <div className="grid grid-cols-9 gap-1">
               {folder.images.map((image, idx) => (
                 <div
                   key={idx}
